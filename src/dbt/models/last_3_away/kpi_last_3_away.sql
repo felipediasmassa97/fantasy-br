3185b62d97cb3993abd6b49dc28197c51acb6528
felipe-massa_TFMC <felipe.massa@technipfmc.com>
2026-02-15 21:33:42 -0300

    Adding this season, last 3 home and away matches filters

---
+++ src/dbt/models/last_3_away/kpi_last_3_away.sql
@@ -0,0 +1,40 @@
+{{ config(materialized='view') }}
+
+with ranked_matches as (
+    select
+        id,
+        name,
+        club,
+        position,
+        round_id,
+        pts_round,
+        row_number() over (partition by id order by round_id desc) as match_rank
+    from {{ ref('int_players') }}
+    where season = 2026 and has_played = true and is_home = false
+),
+
+latest_info as (
+    select id, name, club, position
+    from ranked_matches
+    where match_rank = 1
+),
+
+aggregated as (
+    select
+        id,
+        count(*) as matches_counted,
+        avg(pts_round) as pts_avg
+    from ranked_matches
+    where match_rank <= 3
+    group by id
+)
+
+select
+    a.id,
+    l.name,
+    l.club,
+    l.position,
+    a.matches_counted,
+    a.pts_avg
+from aggregated a
+join latest_info l on a.id = l.id
